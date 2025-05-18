dc8ca2db9208e0e2276e8f18e523c8ea739a6d63
chealeanpenhchakrith <leanpenhchakrith.chea@gmail.com>
2025-05-18 12:14:45 +0200

    Create a NavBar component

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+    <header class="navbar">
+      <h1>Board Right Away</h1>
+      <nav>
+        <router-link to="/">How It Works ?</router-link>
+        <router-link to="/register">Create An Event</router-link>
+        <router-link to="/register">My Space</router-link>
+      </nav>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Navbar'
+  }
+  </script>
+  
+  <style scoped>
+  .navbar {
+    position: fixed;         
+    top: 0;
+    left: 0;
+    width: 100vw;            
+    background: black;
+    color: white;
+    padding: 1rem 2rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    box-sizing: border-box;
+    z-index: 1000;           
+}
+  
+  nav a {
+    color: white;
+    margin-left: 1rem;
+    text-decoration: none;
+  }
+  nav a.router-link-active {
+    text-decoration: underline;
+  }
+  </style>
